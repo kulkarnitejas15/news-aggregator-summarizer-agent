240bf177334e3f0b7293d10a80d7139507d661b5
Tejas Kulkarni <kulkarnitejas864@gmail.com>
2026-02-21 12:59:51 +0530

    fix filterbar categories api

--- Frontend/src/components/FilterBar.jsx
+++ Frontend/src/components/FilterBar.jsx
@@ -7,11 +7,20 @@ const FilterBar = ({ onFilterChange }) => {
 
   // Fetch categories from backend
   useEffect(() => {
-    fetch("http://https://news-backend-gz40.onrender.com/api/articles/categories")
+    const fetchCategories = async () => {
+      try {
+        const API = import.meta.env.VITE_API_BASE_URL;
 
-      .then((res) => res.json())
-      .then((data) => setCategories(data))
-      .catch((err) => console.error("Error fetching categories", err));
+        const res = await fetch(`${API}/api/articles/categories`);
+        const data = await res.json();
+
+        setCategories(data);
+      } catch (err) {
+        console.error("Error fetching categories", err);
+      }
+    };
+
+    fetchCategories();
   }, []);
 
   // Handle filter change
